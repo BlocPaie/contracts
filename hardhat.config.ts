1f15ff0acc6aff4ef048cce4642dfd0623f1b6cc
SamAg19 <sameermg1999@gmail.com>
2026-03-08 22:11:46 +0530

    chore: setup files updated

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@fhevm/hardhat-plugin";
 
 const config: HardhatUserConfig = {
   solidity: {
